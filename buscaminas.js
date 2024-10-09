f84c1f47c135601470003b245b29bfc6114c8c81
AlejandroSolarRuiz <alejandrosolarruiz@gmail.com>
2024-10-09 16:09:40 +0200

    no tocar

--- buscaminas.js
+++ buscaminas.js
@@ -1,29 +1,73 @@
-// preguntar al usuario por la cantidad de filas del tablero
-let filas = prompt("Introduce la cantidad de filas")
-// preguntar al usuario por la cantidad de columnas del tablero
-let columnas = prompt("Introduce la cantidad de columnas")
-// preguntar al usuario por la cantidad de minas del tablero
-let minas = prompt("Introduce la cantidad de minas")
-//Array para guardar las filas y columnas del tablero
-let tablero = [];
+function recibirDatos() {
+    while (true) {
+        finalizar = false;
+        filasOk = false;
+        columnasOk = false;
+    }
+}
 
+// funcion encargada de resolver el tablero, colocando en
+// cada casilla el valor correspondiente a la cantidad de minas
+// adyacentes
 function resolverTablero(tablero) {
+    // por cada fila y por cada columna,
+    // es decir por cada casilla
     for (let i = 0; i < tablero.length; i++) {
         for (let j = 0; j < tablero[0].length; j++) {
-
+            // si la casilla no tiene una mina
+            if (tablero[i][j] != -1) {
+                // invoco a un metodo que me calcula el valor de la casilla
+                calcularCasilla(i, j, tablero);
+            }
         }
     }
 }
 
-// funcion que comprueba que una posicion del tablero sea valida, 
-//para no comprobar casillas que no existan por exceder los limites
-function validarCasilla(fila, columna, tablero) {
-    return (fila > -1 && fila < tablero.length && columna > -1 && columna < tablero[0].length)
+// calcula el valor de una casilla dada
+function calcularCasilla(fila, columna, tablero) {
+    // define las direcciones correspondientes a las 8
+    // posiciones adyacentes
+    let direcciones = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+    ];
+
+    // recorre cada direccion adyacente
+    for (let i = 0; i < direcciones.length; i++) {
+        let nuevaFila = direcciones[i][0];
+        let nuevaColumna = direcciones[i][1];
+
+        // si la casilla adyacente es una casilla valida y tiene una mina
+        if (
+            validarCasilla(nuevaFila, nuevaColumna, tablero) &&
+            tablero[nuevaFila][nuevaColumna] == -1
+        ) {
+            // incremento en 1 el valor de la casilla actual
+            tablero[fila][columna]++;
+        }
+    }
 }
 
+// funcion que comprueba que una posicion del tablero sea valida,
+//para no comprobar casillas que no existan por exceder los limites
+function validarCasilla(fila, columna, tablero) {
+    // devuelve verdadero si la fila y la columna existen en el tablero
+    // son validas las posiciones desde 0 hasta el length-1
+    return (
+        fila > -1 &&
+        fila < tablero.length &&
+        columna > -1 &&
+        columna < tablero[0].length
+    );
+}
 
-
-function  crearTablero (filas,columnas) {
+function crearTablero(filas, columnas) {
     for (let i = 0; i < filas; i++) {
         tablero[i] = [];
         for (let j = 0; j < columnas; j++) {
@@ -36,23 +80,23 @@ let minasColocadas = 0;
 
 function crearMinas(filas, columnas, minas) {
     while (minasColocadas < minas) {
-        let columnasMina = Math.floor((Math.random() * columnas));
+        let columnasMina = Math.floor(Math.random() * columnas);
         let filaMina = Math.floor(Math.random() * filas);
 
-        if (tablero[filaMina][columnasMina] == -1){
+        if (tablero[filaMina][columnasMina] == -1) {
             continue;
-        }else{
+        } else {
             tablero[filaMina][columnasMina] == -1;
             minasColocadas--;
         }
     }
 }
 
-function mostrarTablero(tablero){
+function mostrarTablero(tablero) {
     for (let i = 0; i < tablero.length; i++) {
         for (let j = 0; j < tablero[i].length; j++) {
             document.write(tablero[i][j] + " ");
         }
-        document.write("<br>")
+        document.write("<br>");
     }
 }
