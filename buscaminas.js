3bc8cde8f623b7e3de12a671baad60a0fc2672b0
AlejandroSolarRuiz <alejandrosolarruiz@gmail.com>
2024-10-09 16:04:01 +0200

    hasta ahora todo bien

--- buscaminas.js
+++ buscaminas.js
@@ -69,21 +69,3 @@ function validarCasilla(fila, columna, tablero) {
     );
 }
 
-function crearMinas(filas, columnas, minas) {
-    if ((filas, columnas, minas <= 0)) {
-        alert("Las filas, columnas o minas no pueden ser menor que 0");
-    } else if ((filas, columnas > 9)) {
-        alert("Las filas o columnas no pueden ser mayores de 9");
-    } else if (minas > ((filas * columnas) / 2, 5)) {
-        alert(
-            "Las minas no pueden ser más que la proporcion 1 mina por 2,5 huecos libres"
-        );
-    } else {
-        for (let index = 0; index < minas; index++) {
-            let columnasMina = Math.floor(Math.random() * columnas);
-            let filaMina = Math.floor(Math.random() * filas);
-        }
-    }
-}
-
-console.log("3" < 2);
